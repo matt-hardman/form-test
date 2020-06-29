2c8b08a2d2f43050b768d061b1479df61bd44786
Matt Hardman <matt.hardman@aerian.com>
2020-06-29 11:23:13 +0100

    implement basic form with react-form-hook

--- components/ReactHookForm/ReactHookForm.tsx
+++ components/ReactHookForm/ReactHookForm.tsx
@@ -4,38 +4,66 @@ import { useForm } from "react-hook-form";
 
 interface Inputs {
   email: string;
+  role: string;
 }
 
 const ReactHookForm = () => {
   const { register, handleSubmit, errors } = useForm<Inputs>();
+  const [formData, setFormData] = React.useState<Inputs>();
 
   const onSubmit = (data) => {
-    console.log("submitted");
-    console.log(data);
+    setFormData(data);
   };
 
+  console.log(formData);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-      <input name="email" type="email" ref={register({ required: true })} />
-      {errors.email && <span>"This field is required"</span>}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <div className={styles.email}>
+          <label htmlFor="email">Email Address: </label>
+          <input name="email" type="email" ref={register({ required: true })} />
+          {errors.email && <span>This field is required</span>}
+        </div>
 
-      <label htmlFor="admin">Admin</label>
-      <input
-        name="group"
-        type="radio"
-        value="admin"
-        ref={register}
-        defaultChecked
-      />
+        <div className={styles.role}>
+          <input
+            name="role"
+            type="radio"
+            value="admin"
+            ref={register}
+            defaultChecked
+          />
+          <label htmlFor="admin">Admin</label>
 
-      <label htmlFor="admin">Station Owner</label>
-      <input name="group" type="radio" value="station_owner" ref={register} />
+          <input
+            name="role"
+            type="radio"
+            value="station_owner"
+            ref={register}
+          />
+          <label htmlFor="admin">Station Owner</label>
 
-      <label htmlFor="admin">Insights Only</label>
-      <input name="group" type="radio" value="insights_only" ref={register} />
+          <input
+            name="role"
+            type="radio"
+            value="insights_only"
+            ref={register}
+          />
+          <label htmlFor="admin">Insights Only</label>
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      {formData && (
+        <div>
+          <p>Email: {formData.email}</p>
+          <p>Role: {formData.role}</p>
+        </div>
+      )}
+    </>
   );
 };
 
